e31dac89520af69036c2c6599aa529e73139ad4e
Chamindu-Kasun <66759078+Chamindu-Kasun@users.noreply.github.com>
2021-10-20 16:05:18 +0530

    Welcome section completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./assets/styles/main.scss";
+import {Container,Row,Col} from "react-bootstrap";
+import Welcome from "./components/Welcome";
+import LibraryBody from "./components/LibraryBody";
+import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+  return(
+      <Container fluid={true}>
+          <Row>
+             <Col xs={12}><Welcome/></Col>
+              <Col xs={12}><LibraryBody/></Col>
+              <Col xs={12}><Footer/></Col>
+          </Row>
+      </Container>
+  )
+};
 
 export default App;
