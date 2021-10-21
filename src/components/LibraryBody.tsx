beb5c290568fdab55fea00bec35caa6608683cc7
Chamindu-Kasun <66759078+Chamindu-Kasun@users.noreply.github.com>
2021-10-21 21:44:25 +0530

    Author UX part completed

--- src/components/LibraryBody.tsx
+++ src/components/LibraryBody.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
+import {Row, Col} from "react-bootstrap"
+import Authors from "./authors/Authors";
+import Books from "./books/Books";
 
 const LibraryBody: React.FC = () => {
-    return(<h1>LibraryBody Section</h1>)
 
+    return(
+     <Row className={"mx-2 mt-4 d-flex flex-lg-row flex-md-row flex-column-reverse"}>
+         <Col xs={12} lg={6} md={6}><Books/></Col>
+         <Col xs={12} lg={6} md={6}><Authors/></Col>
+     </Row>
+    )
 }
 
 export default LibraryBody;
